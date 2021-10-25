1446a700cebbe909b16f38f3aa8718e9523bb34d
ByungyeonKim <viva_kby@naver.com>
2021-10-25 15:19:41 +0900

    상세 페이지 원본 URL 크롤링, 엔드포인트 추가

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express();
 const port = 3000;
 const bestRouter = require('./src/api/best');
 const categoryRouter = require('./src/api/category');
+const detailPageRouter = require('./src/api/detail_page');
 
-app.use('/api/best', bestRouter);
 app.use('/api/content', categoryRouter);
+app.use('/api/best', bestRouter);
+app.use('/api/detail', detailPageRouter);
 app.use(express.static('public'));
 
 app.listen(port, () =>
---
+++ src/api/detail_page.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+router.get('/:url', (req, res) => {
+  const getHtml = async () => {
+    const url = req.params.url.replace(/ /g, '/');
+    try {
+      return await axios.get(url);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  getHtml()
+    .then((html) => {
+      const $ = cheerio.load(html.data);
+      const data = [
+        {
+          title: $('div.article_header > div > div > h2').text(),
+          mainContents: $('div.article.d_article').html(),
+          mediaName: $('#btn_media').text(),
+        },
+      ];
+
+      return data;
+    })
+    .then((result) => res.send(result))
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+module.exports = router;
